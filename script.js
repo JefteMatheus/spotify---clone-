210b64014d334c88b53eb391d79a828925d953f5
JefteMatheus <jftmths@gmail.com>
2024-11-29 13:29:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Adicionar funcionalidade nos botões (opcional)
+document.querySelector('.btn.free').addEventListener('click', () => {
+    alert('Spotify Free selecionado!');
+});
+
+document.querySelector('.btn.premium').addEventListener('click', () => {
+    alert('Spotify Premium selecionado!');
+});
